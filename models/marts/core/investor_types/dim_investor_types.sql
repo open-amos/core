0bca85020aadaf893181648292b3840ec456f52c
Amos HQ <benjamin@amos.tech>
2025-10-24 20:55:04 +0200

    Create core dimension: dim_investor_types

---
+++ models/marts/core/investor_types/dim_investor_types.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='table',
+        tags=['marts', 'core', 'dimension']
+    )
+}}
+
+with investor_types as (
+    select
+        investor_type_id,
+        name,
+        description,
+        kyc_category,
+        current_timestamp() as created_at,
+        current_timestamp() as updated_at
+    from {{ ref('stg_ref__investor_types') }}
+)
+
+select * from investor_types
